c1d7e83cb6192e4ff751a8158055742d83043841
gracious_ngetich <76058373+GraciousNgetich@users.noreply.github.com>
2023-07-10 13:44:32 +0300

    Update lambda_fucntion.py

--- lambda_fucntion.py
+++ lambda_fucntion.py
@@ -1,15 +1,5 @@
 """
     Final AWS Lambda function skeleton. 
-    
-    Author: Explore Data Science Academy.
-    
-    Note:
-    ---------------------------------------------------------------------
-    The contents of this file should be added to a AWS  Lambda function 
-    created as part of the EDSA Cloud-Computing Predict. 
-    For further guidance around this process, see the README instruction 
-    file which sits at the root of this repo.
-    ---------------------------------------------------------------------
 
 """
 
